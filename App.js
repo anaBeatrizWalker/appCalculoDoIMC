20da418ee2524d3b757175550de9470b7916ce13
anaBeatrizWalker <anabiawalker@gmail.com>
2023-04-12 07:49:37 -0300

    Alterando aplicação para componente funcional

--- App.js
+++ App.js
@@ -1,66 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet, TextInput, Button, Image } from 'react-native';
 import Constants from 'expo-constants';
 
-export default class App extends Component {
+export default function App(){
 
-  constructor(props){
-    super(props);
-    this.state={
-      peso: 0.0,
-      altura: 0.0,
-      resultado: 0.0,
-      classificacao: ''
-    }
-    this.calcular = this.calcular.bind(this);
-  }
+  const [peso, setPeso] = useState(0.0);
+  const [altura, setAltura] = useState(0.0);
+  const [resultado, setResultado] = useState(0.0);
+  const [classificacao, setClassificacao] = useState("");
 
-  calcular(){
-    result = this.state.peso / (this.state.altura * this.state.altura);
+  function calcular(){
+    let res = peso / (altura * altura);
 
-    if (result < 18.5)
-      texto = 'Você está abaixo do peso.'
-    else if (result < 24.9)
-      texto = 'Você está com o peso normal.'
-    else if (result < 29.9)
-      texto = 'Você está com sobrepeso.'
-    else if (result < 34.9)
-      texto = 'Você está com obesidade grau 1.'
-    else if (result < 39.9)
-      texto = 'Você está com obesidade grau 2.'
+    if (res < 18.5)
+      texto = 'Você está abaixo do peso.';
+    else if (res < 24.9)
+      texto = 'Você está com o peso normal.';
+    else if (res < 29.9)
+      texto = 'Você está com sobrepeso.';
+    else if (res < 34.9)
+      texto = 'Você está com obesidade grau 1.';
+    else if (res < 39.9)
+      texto = 'Você está com obesidade grau 2.';
     else
-      texto = 'Você está com obesidade grau 3 ou mórbida.'
+      texto = 'Você está com obesidade grau 3 ou mórbida.';
 
-    this.setState({
-      resultado: result,
-      classificacao: texto
-    });
+    setResultado(res);
+    setClassificacao(texto);
   }
 
-  render(){
-    return (
-      <View>
-        <Text style={styles.paragraph}>
-          Cálculo do IMC
-        </Text>
+  return (
+    <View>
+      <Text style={styles.paragraph}>
+        Cálculo do IMC
+      </Text>
 
-        <Image style={styles.image} source={require('./IMC-icon.png')}></Image>
+      <Image style={styles.image} source={require('./img/IMC-icon.png')}></Image>
 
-        <TextInput style={styles.inputs}  placeholder="Digite sua altura. Ex: 1.65 " onChangeText={(e) => this.setState({altura:e})}></TextInput>
+      <TextInput style={styles.inputs}  placeholder="Digite sua altura. Ex: 1.65 " onChangeText={(e) => setAltura(e)}></TextInput>
 
-        <TextInput style={styles.inputs}  placeholder="Digite seu peso. Ex: 65 " onChangeText={(e) => this.setState({peso:e})}></TextInput>
+      <TextInput style={styles.inputs}  placeholder="Digite seu peso. Ex: 65 " onChangeText={(e) => setPeso(e)}></TextInput>
 
-        <View style={styles.buttons}>
-          <Button color={'#3CB371'} title='Verificar' onPress={this.calcular}></Button>
-        </View>
-
-        <Text> Altura: {this.state.altura}; Peso: {this.state.peso}; IMC: {this.state.resultado} </Text>
-
-        <Text style={styles.classificacao} className="classificacao"> Classificação: {this.state.classificacao}</Text>
+      <View style={styles.buttons}>
+        <Button color={'#3CB371'} title='Verificar' onPress={calcular}></Button>
       </View>
-    );
-  }
+
+      <Text> Altura: {altura}; Peso: {peso}; IMC: {resultado} </Text>
+
+      <Text style={styles.classificacao} className="classificacao"> Classificação: {classificacao}</Text>
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
   paragraph: {
     margin: 24,
